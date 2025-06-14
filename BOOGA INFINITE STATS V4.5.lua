ee4d0a55085022a0777e65fbd3234520a293237d
FortniBloxYT1 <98220426+FortniBloxYT1@users.noreply.github.com>
2025-06-14 15:28:12 +0200

    Update BOOGA INFINITE STATS V4.5.lua

--- BOOGA INFINITE STATS V4.5.lua
+++ BOOGA INFINITE STATS V4.5.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/8bb0671ecf6bca36d92dc2f721c1c39f.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/cace3ba635743299a369442a84cf30a6.lua"))()
